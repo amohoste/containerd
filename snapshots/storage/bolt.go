d8178aa51914beb4fb33fca156ffdc1499b2b3f4
Amory Hoste <amory.hoste@gmail.com>
2021-08-12 16:40:10 +0200

    expose id as part of stat snapshot

--- snapshots/storage/bolt.go
+++ snapshots/storage/bolt.go
@@ -525,6 +525,7 @@ func readSnapshot(bkt *bolt.Bucket, id *uint64, si *snapshots.Info) error {
 	if si != nil {
 		si.Kind = readKind(bkt)
 		si.Parent = string(bkt.Get(bucketKeyParent))
+		si.SnapshotId = fmt.Sprintf("%d", readID(bkt))
 
 		if err := boltutil.ReadTimestamps(bkt, &si.Created, &si.Updated); err != nil {
 			return err
